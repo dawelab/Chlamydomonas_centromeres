7bc7e40f8047b98601dbe29264a0d63ea766c162
Mingyu-Wang <89274378+Mingyu-Wang@users.noreply.github.com>
2025-04-09 14:47:24 -0400

    Update Step_1_Zepp_identification.sh

--- ZeppL_estimation/Step_1_Zepp_identification.sh
+++ ZeppL_estimation/Step_1_Zepp_identification.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ####== ZeppL identification ==####
-DATADIR="scratch/mw36149/Chlamydomonas_centromere"
+DATADIR="/scratch/mw36149/Chlamydomonas/pre_dotplot"
 mkdir -p $DATADIR/ZeppL_identify && cd $DATADIR/ZeppL_identify
 
 # Load required modules
@@ -29,11 +29,12 @@ for i in ${!GENOMES[@]}; do
     run_blast "${GENOMES[$i]}" "${PREFIXES[$i]}"
 done
 
+cd $DATADIR
 # Function to filter BLAST results
 filter_blast_hits() {
     local prefix=$1
     awk '$3>90 && $4>=300' $DATADIR/ZeppL_identify/${prefix}_ZeppL/ZeppL_pos_${prefix}.txt |
-        awk '{if($9>$10) {print $2"\t"$10"\t"$9} else {print $2"\t"$9"\t"$10}}' | sort -k1,2 > $DATADIR/ZeppL_identify/${prefix}_ZeppL/ZeppL_pos_${prefix}_filtered.bed
+        awk '{if($9>$10) {print $2"\t"$10"\t"$9} else {print $2"\t"$9"\t"$10}}' | sort -k1,1 -k2,2n > $DATADIR/ZeppL_identify/${prefix}_ZeppL/ZeppL_pos_${prefix}_filtered.bed
 }
 
 # Filter BLAST hits for all genomes
@@ -56,15 +57,19 @@ for prefix in "${PREFIXES[@]}"; do
     merge_zeppL_regions "$prefix"
 done
 
-# Manually pick the largest region as ZeppL-enriched region
-# Generate final files: *_ZeppL_v2.bed (manual step needed)
-# copy *_ZeppL_v2.bed to $DATADIR
+### Manually pick the largest region as ZeppL-enriched region and named as *_ZeppL_v2.bed in DATADIR, see below ###
+#cc4532_ZeppL_v2.bed
+#cc5816_ZeppL_v2.bed
+#cc1690_nanopore_ZeppL_v2.bed
+#cc1690_ZeppL_v2.bed
+#cw15_ZeppL_v2.bed
 
-# Get ZeppL coordinates overlapping enriched regions
+# Generate ZeppL_pos_*_filtered_overlap.bed and extract Zepp>8kb
+cd $DATADIR
 extract_zeppL_8kb() {
     local prefix=$1
     bedtools intersect -wa -a $DATADIR/ZeppL_identify/${prefix}_ZeppL/ZeppL_pos_${prefix}_filtered.bed \
-        -b $DATADIR/ZeppL_identify/${prefix}_ZeppL/${prefix}_ZeppL_v2.bed > $DATADIR/ZeppL_identify/${prefix}_ZeppL/ZeppL_pos_${prefix}_filtered_overlap.bed
+        -b $DATADIR/${prefix}_ZeppL_v2.bed > $DATADIR/ZeppL_identify/${prefix}_ZeppL/ZeppL_pos_${prefix}_filtered_overlap.bed
 
     awk '$3-$2>=8000' $DATADIR/ZeppL_identify/${prefix}_ZeppL/ZeppL_pos_${prefix}_filtered_overlap.bed > $DATADIR/ZeppL_identify/${prefix}_ZeppL/ZeppL_pos_${prefix}_8kb.bed
 }
@@ -73,22 +78,3 @@ extract_zeppL_8kb() {
 for prefix in "${PREFIXES[@]}"; do
     extract_zeppL_8kb "$prefix"
 done
-
-# Extract ZeppL > 8kb sequences
-mkdir -p $DATADIR/ZeppL_8kb
-for i in ${!GENOMES[@]}; do
-    bedtools getfasta -fi $DATADIR/${GENOMES[$i]} -bed $DATADIR/ZeppL_identify/${PREFIXES[$i]}_ZeppL/ZeppL_pos_${PREFIXES[$i]}_8kb.bed \
-        -fo $DATADIR/ZeppL_8kb/${PREFIXES[$i]}_8kb_ZeppL.fa
-done
-
-# Compute ZeppL density for Figure 3
-mkdir -p $DATADIR/ZeppL_density && cd $DATADIR/ZeppL_density
-ml bioawk/1.0-GCC-11.2.0
-
-# Generate genome size file
-bioawk -c fastx '{print $name,length($seq)}' $DATADIR/cc1690_HiFi_chr.fa > cc1690_HiFi_chr_size.bed
-
-# Compute ZeppL coverage in 5kb windows
-ml BEDTools/2.30.0-GCC-12.2.0
-bedtools makewindows -g cc1690_HiFi_chr_size.bed -w 5000 > cc1690.fasta.5kb.bed
-bedtools coverage -a cc1690.fasta.5kb.bed -b $DATADIR/ZeppL_pos_cc1690.bed > ZeppL.coverage.5kb.bed
