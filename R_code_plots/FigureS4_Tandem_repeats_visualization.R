f2805f0612c7648fce9764e197ca020f63fd4170
Mingyu-Wang <89274378+Mingyu-Wang@users.noreply.github.com>
2025-04-09 11:40:36 -0400

    FigureS4_Tandem_repeats.R

--- R_code_plots/FigureS4_Tandem_repeats_visualization.R
+++ R_code_plots/FigureS4_Tandem_repeats_visualization.R
@@ -41,9 +41,9 @@ cc1690_TRF_filtered <- cc1690_TRF %>% filter(Copies>400 & Period>125)
 cc1690_TRF_filtered <- cc1690_TRF_filtered %>%
   mutate(order = as.numeric(sub("chr_", "", Chr))) %>% mutate(order=17-order+1)
 
-UL1690_length <- setNames(read.table('cc1690_HiFi_chr_length.txt', sep="\t", fill=TRUE),c("Chr","Length"))
+UL1690_length <- setNames(read.table('UL1690_chr_length.txt', sep="\t", fill=TRUE),c("Chr","Length"))
 UL1690_length <- UL1690_length%>% mutate(order = n() - row_number() + 1) 
-UL1690_gap <- setNames(read.table('cc1690_HiFi_gap.bed', sep="\t", fill=TRUE),c("Chr","Start","End"))
+UL1690_gap <- setNames(read.table('UL1690_gap.bed', sep="\t", fill=TRUE),c("Chr","Start","End"))
 UL1690_gap <- UL1690_gap %>%  mutate(order = as.numeric(sub("chr_", "", Chr))) %>% mutate(order=17-order+1)
 
 cc5816_length <- setNames(read.table('cc5816_Dutcher_length.txt', sep="\t", fill=TRUE),c("Chr","Length"))
@@ -51,17 +51,17 @@ cc5816_length <- cc5816_length%>% mutate(order = n() - row_number() + 1)
 
 cc4532_length <- setNames(read.table('cc4532_v6.1_chr_length.txt', sep="\t", fill=TRUE),c("Chr","Length"))
 cc4532_length <- cc4532_length%>% mutate(order = n() - row_number() + 1) 
-cc4532_gap <- setNames(read.table('CC4532_gap.bed', sep="\t", fill=TRUE),c("Chr","Start","End"))
+cc4532_gap <- setNames(read.table('cc4532_gap.bed', sep="\t", fill=TRUE),c("Chr","Start","End"))
 cc4532_gap <- cc4532_gap %>%  mutate(order = as.numeric(sub("chr_", "", Chr))) %>% mutate(order=17-order+1)
 
 cc1690_length <- setNames(read.table('cc1690_nanopore_length.txt', sep="\t", fill=TRUE),c("Chr","Length"))
 cc1690_length <- cc1690_length%>% mutate(order = n() - row_number() + 1) 
-cc1690_gap <- setNames(read.table('cc1690_nanopore_gap.bed', sep="\t", fill=TRUE),c("Chr","Start","End"))
+cc1690_gap <- setNames(read.table('cc1690_gap.bed', sep="\t", fill=TRUE),c("Chr","Start","End"))
 cc1690_gap <- cc1690_gap %>%  mutate(order = as.numeric(sub("chr_", "", Chr))) %>% mutate(order=17-order+1)
 
 cc400_length <- setNames(read.table('cw15_HiFi_chr_length.txt', sep="\t", fill=TRUE),c("Chr","Length"))
 cc400_length <- cc400_length%>% mutate(order = n() - row_number() + 1) 
-cc400_gap <- setNames(read.table('cw15_HiFi_gap.bed', sep="\t", fill=TRUE),c("Chr","Start","End"))
+cc400_gap <- setNames(read.table('cw15_gap.bed', sep="\t", fill=TRUE),c("Chr","Start","End"))
 cc400_gap <- cc400_gap %>%  mutate(order = as.numeric(sub("chr_", "", Chr))) %>% mutate(order=17-order+1)
 
 UL1690 <- ggplot() +
@@ -79,7 +79,7 @@ UL1690 <- ggplot() +
         axis.title.y = element_blank()          # Hide y-axis title
         )
 UL1690
-ggsave(file="png/UL1690_repeats.png", UL1690, width=25, height=8)
+ggsave(file="FigureS4/UL1690_repeats.png", UL1690, width=25, height=8)
 
 cc5816 <- ggplot() +
   geom_rect(data=cc5816_length,aes(xmin = 0, xmax =Length, ymin=order, ymax = order+0.8),fill="#0072B2",color="black") + 
@@ -95,7 +95,7 @@ cc5816 <- ggplot() +
         axis.title.y = element_blank()          # Hide y-axis title
   )
 cc5816
-ggsave(file="png/cc5816_repeats.png", cc5816, width=25, height=8)
+ggsave(file="FigureS4/cc5816_repeats.png", cc5816, width=25, height=8)
 
 cc400 <- ggplot() +
   geom_rect(data=cc400_length,aes(xmin = 0, xmax =Length, ymin=order, ymax = order+0.8),fill="#0072B2",color="black") + 
@@ -112,7 +112,7 @@ cc400 <- ggplot() +
         axis.title.y = element_blank()          # Hide y-axis title
   )
 cc400
-ggsave(file="png/cc400_repeats.png", cc400, width=25, height=8)
+ggsave(file="FigureS4/cc400_repeats.png", cc400, width=25, height=8)
 
 cc4532 <- ggplot() +
   geom_rect(data=cc4532_length,aes(xmin = 0, xmax =Length, ymin=order, ymax = order+0.8),fill="#0072B2",color="black") + 
@@ -129,7 +129,7 @@ cc4532 <- ggplot() +
         axis.title.y = element_blank()          # Hide y-axis title
   )
 cc4532
-ggsave(file="png/cc4532_repeats.png", cc4532, width=25, height=8)
+ggsave(file="FigureS4/cc4532_repeats.png", cc4532, width=25, height=8)
 
 cc1690 <- ggplot() +
   geom_rect(data=cc1690_length,aes(xmin = 0, xmax =Length, ymin=order, ymax = order+0.8),fill="#0072B2",color="black") + 
@@ -146,8 +146,8 @@ cc1690 <- ggplot() +
         axis.title.y = element_blank()          # Hide y-axis title
   )
 cc1690
-ggsave(file="png/cc1690_repeats.png", cc1690, width=25, height=8)
-
+ggsave(file="FigureS4/cc1690_repeats.png", cc1690, width=25, height=8)
+# Generate information for TableS4
 UL1690_print<- UL1690_TRF_filtered %>% select(Chr, Start,End,Period,Copies,Repeats)
 cc1690_print<- cc1690_TRF_filtered %>% select(Chr, Start,End,Period,Copies,Repeats)
 cc5816_print<- cc5816_TRF_filtered %>% select(Chr, Start,End,Period,Copies,Repeats)
@@ -159,8 +159,3 @@ write.table(cc5816_print, "cc5816_TRF_filtered.txt", sep = "\t", quote = FALSE, 
 write.table(cc400_print, "cc400_TRF_filtered.txt", sep = "\t", quote = FALSE, col.names = FALSE, row.names = FALSE)
 write.table(cc4532_print, "cc4532_TRF_filtered.txt", sep = "\t", quote = FALSE, col.names = FALSE, row.names = FALSE)
 write.table(cc1690_print, "cc1690_TRF_filtered.txt", sep = "\t", quote = FALSE, col.names = FALSE, row.names = FALSE)
-
-cc4532_all_TRF <- setNames(read.table('cc4532_v6.1.fa.2.7.7.80.10.50.2000.dat.bed', sep="\t", fill=TRUE),name_list)
-cc4532_all_TRF_filtered <- cc4532_all_TRF %>% filter(Copies>50 & Period>50)
-cc4532_all_TRF_filtered_contig27 <- cc4532_all_TRF_filtered %>% filter(Chr=="contig_27") %>% select(Chr, Start,End,Copies,Repeats)
-write.table(cc4532_all_TRF_filtered_contig27, "extract_contig27.txt", sep = "\t", quote = FALSE, col.names = FALSE, row.names = FALSE)
